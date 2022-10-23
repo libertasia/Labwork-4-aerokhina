1bc424a492b798b2bac7b5dfdc3983adc87f2931
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-10-23 16:29:21 +0300

    firebase integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'labwork-4-aerokhina',
+    appId: '1:781860722575:web:b1f4513244204b7f795dfa',
+    storageBucket: 'labwork-4-aerokhina.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCAvep5eSssJhXDfsaCAilcO8Hpg84c8zM',
+    authDomain: 'labwork-4-aerokhina.firebaseapp.com',
+    messagingSenderId: '781860722575',
+    measurementId: 'G-CDFMS7HZ1T',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'labwork-4-aerokhina',
+    appId: '1:781860722575:web:b1f4513244204b7f795dfa',
+    storageBucket: 'labwork-4-aerokhina.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCAvep5eSssJhXDfsaCAilcO8Hpg84c8zM',
+    authDomain: 'labwork-4-aerokhina.firebaseapp.com',
+    messagingSenderId: '781860722575',
+    measurementId: 'G-CDFMS7HZ1T',
+  },
   production: false
 };
 
